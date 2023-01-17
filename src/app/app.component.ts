445602dc8a742e7753b6dd37e80c3ed13714c8cd
Andi <andi@zreptil.de>
2023-01-17 16:40:54 +0100

    Auswahl per Mausrad auch dann aktiviert, wenn bisher noch nichts ausgewählt war

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,10 @@ export class AppComponent {
   @HostListener('wheel', ['$event'])
   public onScroll(event: WheelEvent) {
     const diff = Math.sign(event.deltaY);
-    const v = +this.main.paintDef.currentCtrl?.value + diff;
+    let v = +this.main.paintDef.currentCtrl?.value + diff;
+    if (isNaN(v)) {
+      v = 1;
+    }
     const ctrl = this.main.solver?.controls.find(ctrl => ctrl.value === v);
     if (ctrl != null) {
       this.main.paintDef.currentCtrl = ctrl;
